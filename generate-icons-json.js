e3a17e63b024e09da044dee30b4eb7cf1253a8bb
Wildiney Di Masi <wildiney@gmail.com>
2022-07-07 14:24:16 -0300

    add an automated generator for icons resource

---
+++ generate-icons-json.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const fs = require("fs")
+
+const rootPath = path.join(__dirname, 'icons')
+
+function getFilePath (directory) {
+  const paths = []
+  const files = fs.readdirSync(directory)
+  files.forEach((file) => {
+    const directoryPath = path.join(directory, file)
+    paths.push(directoryPath)
+  })
+  return paths
+}
+
+(async () => {
+  const json = []
+  const categories = []
+  const files = getFilePath(rootPath)
+
+  files.forEach((file) => {
+    categories.push(file.split("\\").pop())
+  })
+
+  categories.forEach((category) => {
+    const folder = path.join(rootPath, category)
+    const files = getFilePath(folder)
+    const icons = files.map((file) => {
+      console.log(file)
+      try {
+        const code = fs.readFileSync(file)
+        const name = file.split("\\").pop()
+        const tags = []
+        category.replace('&', '').replace('  ', ' ').split(" ").forEach((category) => {
+          tags.push(category.toLowerCase())
+        })
+
+        name.replace('&', '').replace(".svg", "").split(" ").forEach((name) => {
+          tags.push(name.toLowerCase())
+        })
+        return { name: name, file: code.toString(), tags: tags }
+      } catch (e) {
+        console.log(e)
+      }
+    })
+    json.push({ category: category, icons: icons })
+  })
+
+  console.log(json)
+  fs.writeFileSync('icons.json', JSON.stringify(json), { flag: 'w+' })
+})()
+
